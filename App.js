c5b1f7dc49d0953bb07535408225d60c78e7c532
Nizomiddin-Valijanov <nizomiddinvalijanov71@gmail.com>
2024-01-28 22:41:02 +0500

    first-commit

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, SafeAreaView, Button, Alert } from "react-native";
 
+let helo = 0;
+function salom() {
+  // helo++;
+  // console.log(typeof helo === "number");
+  Alert.alert("Nizomiddin", "Second", [{ text: "yes" }, { text: "No" }]);
+  alert("salom");
+}
+
+function consoleAlert() {
+  if (confirm("Do you okay?")) {
+    alert("good");
+  } else {
+    alert("bad");
+  }
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text} onPress={salom}>
+        hello app!
+      </Text>
+      <Button title="Click me!" onPress={consoleAlert} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "red",
+    width: "100%",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignContent: "center",
+  },
+
+  text: {
+    color: "white",
+    backgroundColor: "green",
+    paddingVertical: "20px",
+    textAlign: "center",
   },
 });
